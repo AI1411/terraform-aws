9f74efc1e8d34f61985547c86a5048ffc17276dc
AI1411 <goethe0719@gmail.com>
2020-02-05 23:58:49 +0900

    iam_roleのモジュール化

--- main.tf
+++ main.tf
@@ -8,6 +8,13 @@ module "web_server" {
   instance_type = "t3.micro"
 }
 
+//module "describe_regions_for_ec2" {
+//  source = "./iam_role"
+//  name = "describe-regions-for-ec2"
+//  identifier = "ec2.amazonaws.com"
+//  policy = data.aws_iam_policy_document.allow_describe_regions.json
+//}
+
 output "public_dns" {
   value = module.web_server.public_dns
 }
