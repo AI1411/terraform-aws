30d658487ac98d602ffee7285c0c7fb58eb0b47e
AI1411 <goethe0719@gmail.com>
2020-02-06 01:40:33 +0900

    S3の設定

---
+++ S3/main.tf
@@ -0,0 +1,71 @@
+provider "aws" {
+  region = "ap-northeast-1"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "private" {
+  bucket = "private-akira-terraform"
+
+  //オブジェクトを変更・削除してもいつでも以前のバージョンへ復元できるようになる
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-akira-terraform"
+  acl = "public-read"
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["https://example.com"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-akira-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      identifiers = ["582318560864"]
+      type = "AWS"
+    }
+  }
+}
